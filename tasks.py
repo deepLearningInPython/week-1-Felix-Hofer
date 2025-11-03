9f93cf3b0d588eb04777903eee984c31b7de898c
Felix-Hofer <felix_hofer@gmx.de>
2025-11-03 23:46:34 +0100

    got rid of the dimension check because I couldn't make it work

--- tasks.py
+++ tasks.py
@@ -54,11 +54,11 @@ def ReLu (arr, cutoff = 0):
 # -----------------------------------------------
 
 def neural_net_layer (twod_arr, oned_arr):
-    if twod_arr.shape[0] == 2 and oned_arr.shape[1] == twod_arr.shape[1]:
+    try:
         result = twod_arr @ oned_arr
         result = ReLu(result)
         return result
-    else:
+    except:
         raise ValueError("Shape mismatch between the arrays")
 
 
